1be8249f2feb0d0da93780ce08d8c8fb36e997b8
prokizzle <nprokesch@gmail.com>
2012-11-13 15:19:46 -0500

    Ignored CSV files

--- roll_smart.rb
+++ roll_smart.rb
@@ -1,4 +1,24 @@
-require_relative 'SmartVisitor.rb'
+ require_relative 'AutoRoller.rb'
 
-@run = SmartVisitor.new(ARGV[0])
-@run.smartRoll
+  lazyCupid = AutoVisitor.new()
+
+  @u = ARGV[0]
+  @p = ARGV[1]
+  @m = ARGV[2].to_i
+
+  puts "Initializing..."
+  lazyCupid.login(@u, @p)
+  # lazyCupid.importCSV(@u)
+  lazyCupid.ignoreUser "james"
+  lazyCupid.loadIgnoreList
+  lazyCupid.loadData
+  # lazyCupid.run(@m, @s)
+  # lazyCupid.lastVisited()
+  lazyCupid.smartRoll(@m)
+  # lazyCupid.stalk()
+  lazyCupid.saveData(@names)
+
+  # singleVisits
+  puts ""
+  puts "Done."
+  puts "---"

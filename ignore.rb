e8164462d3e606e518f7b78ca517059b8273afbb
Nick Prokesch <nprokesch@gmail.com>
2013-02-05 01:54:50 -0500

    deleted old files

--- ignore.rb
+++
@@ -1,6 +0,0 @@
- require_relative 'AutoRoller.rb'
-
-  lazyCupid = AutoVisitor.new()
-  lazyCupid.username = "***REMOVED***"
-  lazyCupid.ignoreUser "#{ARGV[0]}"
-  puts "#{ARGV[0]} added to #{lazyCupid.username}'s ignore list"

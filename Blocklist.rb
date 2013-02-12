880f64f763648d39ccd692a428bef728486a46cd
Nick Prokesch <nprokesch@gmail.com>
2013-02-12 13:44:59 -0500

    changed arguments to hash rockets; removed manual db transactions; added new strategy for blocklists

--- Blocklist.rb
+++ Blocklist.rb
@@ -1,37 +1,39 @@
 
 
 class BlockList
+  attr_accessor :ignore_list, :is_ignored
+  attr_reader :ignore_list, :is_ignored
 
-  def initialize(database)
-    @database = database
-    @counts = @database.data
+  def initialize(args)
+    @database = args[ :database]
+    @ignore_list = @database.ignore
+    process_ignore_list
   end
 
-  def add(user)
-    # if @counts[user].to_i < 1
-        @database.ignore[user] = true
-    # end
-  end
-
-  def ignoreUser(match)
-    @ignore[match] = true
-  end
-
-  def loadIgnoreList
-    begin
-      @ignore = Hash.new(0)
-      CSV.foreach(@username + "_ignore.csv", :headers => true, :skip_blanks => false) do |row|
-        text = row[0]
-        @ignore[text] = true
+  def process_ignore_list
+    @ignore_list.each do |user, value|
+      if value == false
+        @ignore_list.delete(user)
       end
-    rescue
-      ignoreUser(@username)
     end
-
   end
 
-  def checkIgnore? (user)
-    (@ignore[user] == true)
+  def ignore_user(match)
+    @ignore_list[match] = true
+  end
+
+  def save
+    @temp = @database.ignore
+    @temp.each do |user, value|
+      if @ignore_list[user] != value
+        @temp[user] = value
+      end
+    end
+    @database.ignore = @temp
+  end
+
+  def is_ignored (user)
+    (@ignore_list.has_key?(user))
   end
 
 end

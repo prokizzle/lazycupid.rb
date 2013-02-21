a3472b3d624c1bae9b35a6b7ca5814f20036144c
Nick Prokesch <nprokesch@gmail.com>
2013-02-21 00:29:54 -0500

    refactored scraper methods; simplified visitor tracking

--- Harvester.rb
+++ Harvester.rb
@@ -37,46 +37,35 @@ class Harvester
   end
 
   def scrape_from_page(url)
-    @browser.go_to(url)
-    @body = @browser.body
+
+    self.leftbar_scrape(url)
+  end
+
+  def scrape_from_user
     self.leftbar_scrape
+    self.similar_user_scrape
   end
 
   def leftbar_scrape
+    # @browser.go_to(url)
+    @body = @browser.body
     array = @body.scan(/href="\/profile\/([\w\d]+)\?leftbar\_match\=1"/)
     array.each do |users|
       users.each do |user|
-        @database.add_user(user)
+        @database.add_user(user, 0)
       end
     end
   end
 
-  def similar_user_scrape(match)
+  def similar_user_scrape
     # @found = Array.new
-    @database.log(match)
-    @browser.go_to("http://www.okcupid.com/profile/#{match}")
+    # @database.log(match)
+    # @browser.go_to("http://www.okcupid.com/profile/#{match}")
     @body = @browser.body
     array = @body.scan(/href="\/profile\/([\w\d]+)\?cf\=profile\_similar"/)
-    # array.each do |users|
-    #   array.each do |user|
-    #     @browser.go_to("http://www.okcupid.com/profile/#{match}")
-    #     @body = @browser.body
-    #     array = @body.scan(/href="\/profile\/([\w\d]+)\?cf\=profile\_similar"/)
-    #     array.each do |finds|
-    #       array.each do |find|
-    #         @found += [find]
-    #       end
-    #     end
-    #   end
-    # end
-
-    # @found.each do |finder|
-    #   @database.add_new_match(finder)
-    # end
-
     array.each do |users|
       users.each do |user|
-        @database.add_user(user)
+        @database.add_user(user, 0)
       end
     end
   end
@@ -93,14 +82,16 @@ class Harvester
     @visitors = @current_user.parser.xpath("//div[@id='main_column']").to_html
     array = @visitors.scan(/"usr-([\w\d]+)".+z\-index\:\s(\d\d\d)/)
     array.each do |visitor, zindex|
-      @timestamp_block = @current_user.parser.xpath("//div[@id='usr-#{visitor}-info']/p/span[@class='fancydate']").to_html
-      @timestamp = @timestamp_block.match(/\d+/)[0]
-      if @database.get_visitor_timestamp(visitor) != @timestamp.to_i
+      @timestamp_block = @current_user.parser.xpath("//div[@id='usr-#{visitor}-info']/p/script/text()").to_html
+      @timestamp = @timestamp_block.match(/(\d{10}), 'JOU/)[1].to_i
+      @stored_timestamp = @database.get_visitor_timestamp(visitor).to_i
+      if !(@stored_timestamp == @timestamp)
         self.increment_visitor_counter(visitor)
         puts "New visit from #{visitor}"
-        puts "Timestamp: #{@timestamp} & Stored: #{@database.get_visitor_timestamp(visitor)}"
+        # puts "Timestamp: #{@timestamp} & Stored: #{@database.get_visitor_timestamp(visitor)}"
       end
-      @database.set_visitor_timestamp(visitor, @timestamp)
+      @database.set_visitor_timestamp(visitor, @timestamp.to_i)
+      @database.add_user(visitor, 0)
     end
     sleep 1
   end

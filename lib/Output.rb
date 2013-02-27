7f42e9c3b72b0d013e9b1fc57d305998ef5c97cb
Nick Prokesch <nprokesch@gmail.com>
2013-02-26 19:57:00 -0500

    initial state abbreviation support

--- lib/Output.rb
+++ lib/Output.rb
@@ -44,6 +44,71 @@ class Output
 
   end
 
+  def state_abbr(state)
+    @state_abbr[state]
+  end
+
+  @state_abbr = {
+    'Alabama' =>'AL',
+    'Alaska' =>'AK',
+    'America Samoa' =>'AS',
+    'Arizona' =>'AZ',
+    'Arkansas' =>'AR',
+    'California' =>'CA',
+    'Colorado' =>'CO',
+    'Connecticut' =>'CT',
+    'Delaware' =>'DE',
+    'District of Columbia' =>'DC',
+    'Micronesia1' =>'FM',
+    'Florida' =>'FL',
+    'Georgia' =>'GA',
+    'Guam' =>'GU',
+    'Hawaii' =>'HI',
+    'Idaho' =>'ID',
+    'Illinois' =>'IL',
+    'Indiana' =>'IN',
+    'Iowa' =>'IA',
+    'Kansas' =>'KS',
+    'Kentucky' =>'KY',
+    'Louisiana' =>'LA',
+    'Maine' =>'ME',
+    'Islands1' =>'MH',
+    'Maryland' =>'MD',
+    'Massachusetts' =>'MA',
+    'Michigan' =>'MI',
+    'Minnesota' =>'MN',
+    'Mississippi' =>'MS',
+    'Missouri' =>'MO',
+    'Montana' =>'MT',
+    'Nebraska' =>'NE',
+    'Nevada' =>'NV',
+    'New Hampshire' =>'NH',
+    'New Jersey' =>'NJ',
+    'New Mexico' =>'NM',
+    'New York' =>'NY',
+    'North Carolina' =>'NC',
+    'North Dakota' =>'ND',
+    'Ohio' =>'OH',
+    'Oklahoma' =>'OK',
+    'Oregon' =>'OR',
+    'Palau' =>'PW',
+    'Pennsylvania' =>'PA',
+    'Puerto Rico' =>'PR',
+    'Rhode Island' =>'RI',
+    'South Carolina' =>'SC',
+    'South Dakota' =>'SD',
+    'Tennessee' =>'TN',
+    'Texas' =>'TX',
+    'Utah' =>'UT',
+    'Vermont' =>'VT',
+    'Virgin Island' =>'VI',
+    'Virginia' =>'VA',
+    'Washington' =>'WA',
+    'West Virginia' =>'WV',
+    'Wisconsin' =>'WI',
+    'Wyoming' =>'WY'
+  }
+
 
   # def p(i)
   #   puts i

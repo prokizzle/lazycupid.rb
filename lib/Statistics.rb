60cff11d9afe38107fe64610e8e61f2bb18d4be7
Nick Prokesch <nprokesch@gmail.com>
2013-03-07 12:29:01 -0500

    new classes for Settings and shared data storage

---
+++ lib/Statistics.rb
@@ -0,0 +1,21 @@
+class Statistics
+  attr_reader :visitors, :start_time, :visited
+  attr_accessor :visitors, :start_time, :visited
+
+  def initialize
+
+  end
+
+  def visitors
+    @visitors
+  end
+
+  def visited
+    @visited
+  end
+
+  def start_time
+    @start_time
+  end
+
+end

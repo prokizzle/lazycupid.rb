a6a4345789729dbc5d81ec247773810641580bf8
Nick Prokesch <nprokesch@gmail.com>
2013-04-29 15:44:19 -0400

    code cleanup

--- browser_test.rb
+++ browser_test.rb
@@ -1,21 +1,23 @@
 require './includes'
 
-    class Settings2
-    attr_reader :verbose, :debug
-  end
+class Settings2
+  attr_reader :verbose, :debug
+end
 
-  @config = Settings2.new
+@config = Settings2.new
 
-    @username = "DanceYrselfCln"
+@username = ARGV[0]
+@password = ARGV[1]
 
-    log_path      = File.dirname($0) + '/logs/'
-    db            = DatabaseMgr.new(:login_name => @username, :settings => @config)
-    @log          = Logger.new("logs/test_#{Time.now}.log")
-    @browser      = Session.new(:username => @username, :password => "123457", :path => log_path, :log => @log)
-    @user         = Users.new(:database => db, :browser => @browser, :log => @log, :path => log_path)
+log_path      = File.dirname($0) + '/logs/'
+db            = DatabaseMgr.new(:login_name => @username, :settings => @config)
+@log          = Logger.new("logs/test_#{Time.now}.log")
+@browser      = Session.new(:username => @username, :password => @password, :path => log_path, :log => @log)
+@user         = Users.new(:database => db, :browser => @browser, :log => @log, :path => log_path)
 
-    @browser.login
-    result = @user.profile("***REMOVED***")
-    hash = @browser.hash
-    puts hash
-    puts result
+@browser.login
+result = @user.profile("***REMOVED***")
+hash = @browser.hash
+puts hash
+puts result
+@browser.logout

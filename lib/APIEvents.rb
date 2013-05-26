c98d945b689838d1304fa31f4fbbecaac904e7ae
Nick Prokesch <nprokesch@gmail.com>
2013-05-26 18:43:11 -0400

    growl alerts for new visits & new messages

--- lib/APIEvents.rb
+++ lib/APIEvents.rb
@@ -9,30 +9,43 @@ class APIEvents
     @spotlight  = Hash.new
     @messages   = Hash.new
     @stalks     = Hash.new
+
+    @g = Growl.new "localhost", "LazyCupid"
+    @g.add_notification "lazy-cupid-notification"
+
+
   end
 
-  def process(event, people)
+  def process(event, people, key)
     @event = event
     @people = people
+    @key = key
     type = event["type"]
     self.send(type)
   end
 
   def msg_notify
-      puts "New message from #{@event["screenname"]}" #unless @event["server_gmt"] == @gmt
-    unless @messages.has_key?(@event["server_gmt"])
-      @tracker.register_message(@event["screenname"], this_event_time)
-    end
-    @messages[@event["server_gmt"]] = @people["screenname"]
+    # unless Time.now.to_i - @last_call <= 1
+      p @event
+      key = "#{@event['server_gmt']}#{@event['from']}"
+      p key
+      unless @messages.has_key?(key)
+        @g.notify "lazy-cupid-notification", @tracker.account, "New message from #{@people['screenname']}"
+        puts "New message from #{@event["from"]}"
+        @tracker.register_message(@event["from"], this_event_time)
+      end
+      @messages[key] = "#{@event['server_gmt']}#{@event['from']}"
+      @last_call = Time.now.to_i
+    # end
   end
 
   def looks_vote
-    @log.debug "looks_vote: #{@event["screenname"]}"
+    @log.debug "looks_vote: #{@event["from"]}"
   end
 
   def im
     # unless @events_hash.has_key(this_event_time)
-    puts "New IM from #{@event["screenname"]}" #unless @event["server_gmt"] == @gmt
+    puts "New IM from #{@event["from"]}" #unless @event["server_gmt"] == @gmt
     # @tracker.register_message(@event["screenname"], @event["server_gmt"])
     # end
     # @events_hash[this_event_time] = @event["screenname"]
@@ -43,6 +56,7 @@ class APIEvents
     unless @stalks.has_key?(@event["server_gmt"])
       # puts "New visit from #{@event['screenname']}"
       @tracker.register_visit(@people)
+      @g.notify "lazy-cupid-notification", @tracker.account, "New visit from #{@people['screenname']}"
     end
     @stalks[@event["server_gmt"]] = @people["screenname"]
   end
@@ -51,7 +65,7 @@ class APIEvents
     handle_ = @people["screenname"]
     gmt_ = @event["server_gmt"]
     unless @spotlight.has_key?(gmt_)
-      puts "New spotlight user: #{handle_} (#{@people["gender"]})" #if verbose
+      # puts "New spotlight user: #{handle_} (#{@people["gender"]})" #if verbose
       @tracker.add_user(handle_, @people["gender"])
       @spotlight[gmt_] = handle_
     end

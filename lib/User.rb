fab1157ad7ebe9e8a6518c1f53dfcc17d9624f94
Nick Prokesch <nprokesch@gmail.com>
2013-05-26 18:46:23 -0400

    bug fix for missing percentages on http; profile match preference scraping

--- lib/User.rb
+++ lib/User.rb
@@ -11,6 +11,20 @@ class Users
     @debug    = true
   end
 
+
+  def match_preferences
+    r = @browser.body_of("http://www.okcupid.com/profile", Time.now.to_i)
+    gentation = r[:html].parser.xpath("//li[@id='ajax_gentation']").to_html
+    ages = r[:html].parser.xpath("//li[@id='ajax_ages']").to_html
+    location = r[:html].parser.xpath("//span[@id='ajax_location']")
+    @looking_for = /(\w+) who like/.match(gentation)[1]
+    @min_age = (/(\d{2}).+(\d{2})/).match(ages)[1]
+    @max_age = (/(\d{2}).+(\d{2})/).match(ages)[2]
+    @my_city = (/[\w\s]+,\s([\w\s]+)/).match(location)[1]
+    # <li id="ajax_ages">Ages 20&ndash;35</li>
+    {min_age: @min_age, max_age: @max_age, city: @my_city, looking_for: @looking_for}
+  end
+
   # def verbose
   #   @verbose
   # end
@@ -123,16 +137,28 @@ class Users
       new_result.to_i
     rescue
       log.debug "match_percentage: #{body}"
-      @body.match(/(\d+). Match/)[1].to_i
+      begin
+        @body.match(/(\d+). Match/)[1].to_i
+      rescue
+        0
+      end
     end
   end
 
   def friend_percentage
-    />(\d+). Friend.*/.match(@body)[1].to_i
+    begin
+      />(\d+). Friend.*/.match(@body)[1].to_i
+    rescue
+      0
+    end
   end
 
   def enemy_percentage
-    />(\d+). Enemy.*/.match(@body)[1].to_i
+    begin
+      />(\d+). Enemy.*/.match(@body)[1].to_i
+    rescue
+      0
+    end
   end
 
   def slut_test_results

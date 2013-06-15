0782351eb90acde15f4dbf3b6e6c0413b6f8172e
Nick Prokesch <nprokesch@gmail.com>
2013-06-15 05:48:31 -0400

    display enemy percentage

--- lib/Output.rb
+++ lib/Output.rb
@@ -36,7 +36,7 @@ class Output
 
   def log(user, added_from)
     print @username
-    result = {user: user[:handle], match_percent: user[:match_percentage], age: user[:age], city: user[:city], state: user[:state], distance: user[:distance], count: @you.visited(user[:handle]), last_online: Time.at(user[:last_online]||0).ago.to_words, added_from: added_from}
+    result = {user: user[:handle], match_percent: user[:match_percentage], age: user[:age], city: user[:city], state: user[:state], distance: user[:distance], enemy: user[:enemy_percentage], count: @you.visited(user[:handle]), last_online: Time.at(user[:last_online]||0).ago.to_words, added_from: added_from, prev_visit: @you.prev_visit(user[:handle])}
     puts result
   end
 

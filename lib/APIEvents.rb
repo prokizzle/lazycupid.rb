069f5090bf2d8385e98afd94118246f0f1d28339
Nick Prokesch <nprokesch@gmail.com>
2013-07-09 15:14:30 -0400

    added fix for errors in API

--- lib/APIEvents.rb
+++ lib/APIEvents.rb
@@ -22,22 +22,36 @@ class APIEvents
     @people = people
     @key = key
     @api_event = Hash.new(event: event, people: people, key:key)
-    type = event["type"]
-    self.send(type)
+    # print formatted_time
+    unless invalid_event
+      begin
+        self.send(event["type"])
+      rescue Exception => e
+        puts "Error #{event}", e.message, e.backtrace
+      end
+    end
+  end
+
+  def formatted_time
+    "#{Time.now.hour}:#{Time.now.min}"
+  end
+
+  def invalid_event
+    @event["from"] == "0" || @people.nil? || @people == {}
   end
 
   def msg_notify
     # unless Time.now.to_i - @last_call <= 1
-      p @event
-      key = "#{@event['server_gmt']}#{@event['from']}"
-      p key
-      unless @messages.has_key?(key)
-        @g.notify "lazy-cupid-notification", "New Message", "#{@people['screenname']}" if @settings.growl_new_mail
-        puts "New message from #{@event["from"]}"
-        @tracker.register_message(@event["from"], this_event_time)
-      end
-      @messages[key] = "#{@event['server_gmt']}#{@event['from']}"
-      @last_call = Time.now.to_i
+    p @event
+    key = "#{@event['server_gmt']}#{@event['from']}"
+    p key
+    unless @messages.has_key?(key)
+      # @g.notify "lazy-cupid-notification", "New Message", "#{@people['screenname']}" if @settings.growl_new_mail
+      puts "New message from #{@event["from"]}"
+      @tracker.register_message(@event["from"], @event["server_gmt"], @people["gender"])
+    end
+    @messages[key] = "#{@event['server_gmt']}#{@event['from']}"
+    @last_call = Time.now.to_i
     # end
   end
 

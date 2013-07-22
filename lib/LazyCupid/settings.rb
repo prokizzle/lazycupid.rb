57ee5311a56f081647e3e23597e44eb89b314e19
Nick Prokesch <nprokesch@gmail.com>
2013-07-22 17:42:05 -0400

    exits app after first run for proper config options

--- lib/LazyCupid/settings.rb
+++ lib/LazyCupid/settings.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module LazyCupid
   class Settings
     attr_reader :max_distance,
@@ -18,8 +20,23 @@ module LazyCupid
       @account  = args[ :username]
       path      = args[ :path]
       @filename = "#{path}/#{@account}.yml"
+
       @db_file  = "#{path}/database.yml"
-      @browser     = args[ :browser]
+      # @browser     = args[ :browser]
+      unless File.exists?(@db_file)
+        db_ = {
+          development: {
+            adapter: "postgresql",
+            host: "localhost",
+            username: "postgres",
+            password: "123456",
+            database: "lazy_cupid"
+          }
+        }
+        File.open(@db_file, "w") do |f|
+          f.write(db_.to_yaml)
+        end
+      end
       unless File.exists?(@filename)
         config = {geo: {
                     distance_filter_type: "distance",
@@ -66,36 +83,11 @@ module LazyCupid
         File.open(@filename, "w") do |f|
           f.write(config.to_yaml)
         end
-      end
-      unless File.exists?(@db_file)
-        db_ = {
-          development: {
-            adapter: "postgresql",
-            host: "localhost",
-            username: "postgres",
-            password: "123456",
-            database: "lazy_cupid"
-          }
-        }
-        File.open(@db_file, "w") do |f|
-          f.write(db_.to_yaml)
-        end
+        puts "Exiting... restart app after setting your config preferences"
+        exit
       end
       @settings = YAML.load_file(@filename)
       @db_settings = YAML.load_file(@db_file)
-    end
-
-    def match_preferences
-      r = @browser.body_of("http://www.okcupid.com/profile", Time.now.to_i)
-      gentation = r[:html].parser.xpath("//li[@id='ajax_gentation']").to_html
-      ages = r[:html].parser.xpath("//li[@id='ajax_ages']").to_html
-      location = r[:html].parser.xpath("//span[@id='ajax_location']")
-      @looking_for = /(\w+) who like/.match(gentation)[1]
-      @min_age = (/(\d{2}).+(\d{2})/).match(ages)[1]
-      @max_age = (/(\d{2}).+(\d{2})/).match(ages)[2]
-      @my_city = (/[\w\s]+,\s([\w\s]+)/).match(location)[1]
-      # <li id="ajax_ages">Ages 20&ndash;35</li>
-      {min_age: @min_age, max_age: @max_age, city: @my_city, looking_for: @looking_for}
     end
 
     def reload_settings

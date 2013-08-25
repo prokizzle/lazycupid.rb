3420aa4324cd40dd31e015d658b5b683ed49b8a9
Nick Prokesch <nprokesch@gmail.com>
2013-08-25 01:21:49 -0400

    added login status detection with login messages

--- lib/LazyCupid/browser.rb
+++ lib/LazyCupid/browser.rb
@@ -40,36 +40,33 @@ module LazyCupid
     # @return [Boolean] True if account has been logged in successfully
     #
     def login
-      begin
-        @agent.keep_alive               = false
-        @agent.idle_timeout             = 5
-        @agent.read_timeout             = 2
-        @agent.user_agent_alias         = ['Mac Safari', 'Mac Firefox'].sample
-        @agent.agent.http.debug_output  = $stderr if debug
-        page = @agent.get("http://www.okcupid.com/")
-        form = page.forms.first
-        form['username']                = @username
-        form['password']                = @password
-        page                            = form.submit
-        @page                           = page
-        sleep 1
-      rescue Exception => e
-        puts e.backtrace
-        puts "Invalid password. Please try again"
-      end
       # Mechanize settings
+      agent.keep_alive               = false
+      agent.idle_timeout             = 5
+      agent.read_timeout             = 2
+      agent.user_agent_alias         = ['Mac Safari', 'Mac Firefox'].sample
+      agent.agent.http.debug_output  = $stderr if debug
+
       # OKCupid login sequence
+      @page                           = agent.get("http://www.okcupid.com/")
+      form                            = page.forms.first
+      form['username']                = @username
+      form['password']                = @password
+      @page                           = form.submit
+      sleep 1
+
       # Check if logged in
+      @page_source                    = @page.parser.xpath("//html").to_html.to_s
+      @login_status                   = check_session_status
       is_logged_in?
     end
 
     # Determines if captcha is required to login
-    # 
+    #
     # @return [Boolean] True if captcha has been detected on login page
-    # 
+    #
     def recaptcha?
-      result = @page.parser.xpath("//body").to_html
-      result.match(/recaptcha_only_if_image/)
+      (page_source =~ /recaptcha_only_if_image/).swag?
     end
 
     # Logs out / ends OKCupid login session
@@ -80,25 +77,43 @@ module LazyCupid
     end
 
     # Determines if account has logged in successfully
-    # 
+    #
     # @return [Boolean] True if account has been logged in
-    # 
+    #
     def is_logged_in?
-      response = body_of("http://www.okcupid.com/home?cf=logo", Time.now.to_i)
-      /logged_in/.match(response[:body])
+      (page_source =~ /loggedin/).swag? rescue nil
     end
 
-    # Removes a html response hash from the hash of responses
-    # 
-    # @param key [Integer] original timestamp used to request a page
-    # @return [Boolean] True if response has been deleted
-    # 
-    def delete_response(key)
-      @hash.tap {|k| k.delete(key)}
     # Determines status of OKCupid session
     # Useful for login interaction screen
     #
     # @return String status of login attempt
+    def check_session_status
+      # @page         = agent.get("http://www.okcupid.com/")
+      # @page_source  = @page.parser.xpath("//html").to_html.to_s
+
+      if is_logged_in?
+        "Logged in"
+      elsif wrong_password?
+        "Incorrect username or password"
+      elsif is_deleted?
+        "Account has been deleted"
+      elsif is_deactivated?
+        "Account has been deactivated"
+      elsif recaptcha?
+        "Recaptcha has been detected. Login via browser first"
+      else
+        "Unknown login error"
+      end
+    end
+
+    def is_deactivated?
+      (page_source =~ /Restore your account/).swag?
+    end
+
+    def wrong_password?
+      # p page_source
+      (page_source =~ /your info was incorrect/).swag?
     end
 
     # Determines if OKCupid login session has been ended
@@ -106,8 +121,7 @@ module LazyCupid
     # @return [Boolean] True is account has been logged out
     #
     def is_logged_out?
-      response = body_of("http://www.okcupid.com/", Time.now.to_i)
-      /logged_out/.match(response[:body])
+      (page_source =~ /logged_out/).swag?
     end
 
     # Determines if logged in account has been deleted by moderators    #
@@ -115,24 +129,29 @@ module LazyCupid
     # @return [Boolean] [True if account has been deleted]
     #
     def is_deleted?
-      response = body_of("http://www.okcupid.com/home?cf=logo", Time.now.to_i)
-      /has been deleted/.match(response[:body])
+      (page_source =~ /account was deleted/).swag?
+    end
+
+
     # Removes a html response hash from the hash of responses
     #
     # @param key [Integer] original timestamp used to request a page
     # @return [Boolean] True if response has been deleted
     #
+    def delete_response(key)
+      @hash.tap {|k| k.delete(key)}
     end
 
+
     # Visits URL and returns HTML body
-    # 
+    #
     # @param link [String] [URL of html page to return]
     # @return [String] [HTML value of body element]
-    # 
+    #
     def go_to(link)
       @url = link
       # begin
-      @current_user = @agent.get(link)
+      @current_user = agent.get(link)
       # @log.debug "#{@url}"
       @body = @current_user.parser.xpath("//body").to_html
       # rescue
@@ -140,36 +159,36 @@ module LazyCupid
     end
 
     def body_of(link, request_id)
-      @hash[request_id] = {ready: false}
-      url = URI.escape(link)
-      # @agent.read_timeout=30
-      temp = @agent.get(url)
+      @hash[request_id]       = {ready: false}
+      url                     = URI.escape(link)
+      # agent.read_timeout=30
+      page_object = agent.get(url)
       # @log.debug "#{@url}"
-      returned_body = temp.parser.xpath("//body").to_html
-      @hash[request_id] = {url: url.to_s, body: returned_body.to_s, html: temp, ready: true}
-      {url: url.to_s, body: returned_body.to_s, html: temp, hash: request_id.to_i}
+      page_body = page_object.parser.xpath("//html").to_html
+      @hash[request_id] = {url: url.to_s, body: page_body.to_s, html: page_object, ready: true}
+      {url: url.to_s, body: page_body.to_s, html: page_object, hash: request_id.to_i}
     end
 
     # Initiates a http request via Mechanize agent
-    # 
+    #
     # @param link       [String] [URL of page to get]
     # @param request_id [Integer] [timestamp to identify and retrieve returned data]
     # @return [Boolean]
-    # 
+    #
     def send_request(link, request_id)
-      # request_id = Time.now.to_i
-      @hash[request_id] = {ready: false}
-      url = URI.escape(link)
-      @agent.read_timeout=30
-      temp = @agent.get(link)
-      # @log.debug "#{@url}"
-      returned_body = temp.parser.xpath("//body").to_html
-      @hash[request_id] = {url: url.to_s, body: returned_body.to_s, html: temp, ready: true}
+      # request_id          = Time.now.to_i
+      @hash[request_id]     = {ready: false}
+      url                   = URI.escape(link)
+      agent.read_timeout    = 30
+      page_object           = agent.get(link)
+      page_body             = page_object.parser.xpath("//body").to_html
+      page_source           = page_object.parser.xpath("//html").to_html
+      @hash[request_id]     = {url: url.to_s, body: page_body.to_s, html: page_object, ready: true, source: page_source}
       true
     end
 
     # Retrieves a hash of attributes parsed from a page
-    # 
+    #
     # @param request_id [Integer] timestamp value originally passed to send_request method
     # @return [Hash] A hash containing strings and objects with the requested page data
     #   url: a String representing the original URL passed to the getter
@@ -177,33 +196,33 @@ module LazyCupid
     #   html: An unaltered Mechanize page object
     #   ready: Boolean value indicating whether or not Mechanize has finished loading
     #           the page into memory
-    # 
+    #
     def get_request(request_id)
       return @hash[request_id]
     end
 
     # Initiates a http request via Mechanize agent
-    # 
+    #
     # @param link       [String] [URL of page to get]
     # @param request_id [Integer] [timestamp to identify and retrieve returned data]
     # @return [Boolean]
-    #  
+    #
     def request(link, request_id)
       @hash[request_id] = {ready: false}
       url = URI.escape(link)
-      @agent.read_timeout=30
-      temp = @agent.get(link)
+      agent.read_timeout=30
+      page_object = agent.get(link)
       # @log.debug "#{@url}"
-      returned_body = temp.parser.xpath("//body").to_html
-      @hash[request_id] = {url: url.to_s, body: returned_body.to_s, html: temp, ready: true}
-      {url: url.to_s, body: returned_body.to_s, html: temp, hash: request_id.to_i}
+      page_body = page_object.parser.xpath("//body").to_html
+      @hash[request_id] = {url: url.to_s, body: page_body.to_s, html: page_object, ready: true}
+      {url: url.to_s, body: page_body.to_s, html: page_object, hash: request_id.to_i}
     end
 
     def html_of(link, request_id)
       url = URI.escape(link)
-      temp = @agent.get(url)
+      page_object = agent.get(url)
       # @log.debug "#{@url}"
-      {url: url, html: temp, hash: request_id}
+      {url: url, html: page_object, hash: request_id}
     end
 
 
@@ -217,6 +236,10 @@ module LazyCupid
     # @return [Boolean]
     def logout
       go_to("http://www.okcupid.com/logout")
+    end
+
+    def page_source
+      @page.parser.xpath("//html").to_html.to_s
     end
 
     # Detects an invalid account or profile

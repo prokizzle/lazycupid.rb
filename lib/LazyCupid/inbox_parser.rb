364462df4a807a6e67a508be210c287f83b2decb
Nick Prokesch <nprokesch@gmail.com>
2013-10-17 02:45:42 -0400

    code cleanup
    Former-commit-id: 5fb6f73d54827fcfc443e19d2b761fd7cce12d92

--- lib/LazyCupid/inbox_parser.rb
+++ lib/LazyCupid/inbox_parser.rb
@@ -4,15 +4,21 @@ module LazyCupid
 
   class InboxScraper
 
-    def async_response(url)
-      result = {ready: false}
-      request_id = UUID.create_timestamp
-      @browser.send_request(url, request_id)
-      until result[:ready] == true
-        result = @browser.get_request(request_id)
+    def initialize(args)
+      # @page_source = args[:page_object][:html]
+      @browser              = args[:browser]
+      @db                   = args[:database]
+      @prev_total_messages  = -1
+    end
+
+    def parse_inbox_page(url)
+      result = async_response(url)
+      message_list = result[:body].scan(/id\="message_(\d+)"/)
+      message_list.each do |id|
+        thread = result[:html].parser.xpath("//li[@id='message_#{id[0]}']")
+        info = thread.to_html.match(/([-\w\d_]+)\?cf=messages..class="photo">.+src="(.+)" border.+threadid.(\d+).+fancydate_\d+.. (\d+),/)
+        @messages.push({handle: info[1], photo_thumbnail: info[2], thread_id: info[3], thread_url: "http://www.okcupid.com/messages?readmsg=true&threadid=#{info[3]}&folder=1", message_date: info[4]})
       end
-      @browser.delete_response(request_id)
-      return result
     end
 
     def analyze_message_thread(thread)
@@ -26,31 +32,33 @@ module LazyCupid
       puts thread
     end
 
-    def parse_inbox_page(url)
-      result = async_response(url)
-      message_list = result[:body].scan(/id\="message_(\d+)"/)
-      message_list.each do |id|
-        thread = result[:html].parser.xpath("//li[@id='message_#{id[0]}']")
-        info = thread.to_html.match(/([-\w\d_]+)\?cf=messages..class="photo">.+src="(.+)" border.+threadid.(\d+).+fancydate_\d+.. (\d+),/)
-        @messages.push({handle: info[1], photo_thumbnail: info[2], thread_id: info[3], thread_url: "http://www.okcupid.com/messages?readmsg=true&threadid=#{info[3]}&folder=1", message_date: info[4]})
-      end
-    end
-
     def scrape_inbox
-      puts "Scraping inbox" if verbose
+      puts "Scraping inbox" if $verbose
       result = async_response("http://www.okcupid.com/messages")
+      # begin
       begin
-        @total_msg    = result[:body].match(/"pg_total.>(\d+)</)[1].to_i
+        @total_msg = result[:body].match(/Page 1 of <a href="\/messages\?low\=(\d+)\&amp\;folder\=1">\d+/)[1].to_i
       rescue
-        @total_msg    = 0
+        @total_msg = 0
       end
-      puts "Total messages: #{@total_msg}" if verbose
+      # @total_msg    = total_pages * 30
+      # rescue
+      # @total_msg    = 0
+      # end
+      puts "Total messages: #{@total_msg}" if $verbose
       sleep 2
       unless @total_msg == @prev_total_messages
-        puts "#{@total_msg - @prev_total_messages} new messages..." if @total_msg > 0
         track_msg_dates("http://www.okcupid.com/messages")
+        if @total_msg > 0
+          puts "#{@total_msg - @prev_total_messages} new messages..."
+        else
+          puts @total_msg_on_page
+          @total_msg = @total_msg_on_page
+        end
         low = 31
         until low >= @total_msg
+          # puts "Scraping inbox: #{((low.to_f/@total_msg.to_f)*100).to_i}%" if $debug
+          # puts low if $debug
           low += 30
           track_msg_dates("http://www.okcupid.com/messages?low=#{low}&folder=1")
           sleep (1..6).to_a.sample.to_i
@@ -59,9 +67,32 @@ module LazyCupid
       end
     end
 
+    def track_msg_dates(msg_page)
+      result = async_response(msg_page)
+      message_list = result[:body].scan(/"message_(\d+)"/)
+      @total_msg_on_page = message_list.size
+      message_list.each do |message_id|
+        p message_id
+        message_id      = message_id[0]
+        puts message_id
+        msg_block       = result[:html].parser.xpath("//li[@id='message_#{message_id}']").to_html
+        # puts msg_block
+        sender          = /\/([\w\d_-]+)\?cf=messages/.match(msg_block)[1]
+        timestamp_block = result[:html].parser.xpath("//li[@id='message_#{message_id.to_s}']/span/script/text()").to_html
+        timestamp       = timestamp_block.match(/(\d{10}), 'MAI/)[1].to_i
+        sender          = sender.to_s
+        gender          = "Q"
+
+        register_message(sender, timestamp, gender)
+
+      end
+    end
+
     def register_message(sender, timestamp, gender)
       # @stored_time     = @db.get_last_received_message_date(sender).to_i
-      puts "New message from #{sender}"
+      if timestamp.to_i >= Time.now.to_i - 1800
+        puts "New message from #{sender}"
+      end
 
       @db.add_user(sender, gender, "inbox")
       @db.ignore_user(sender)
@@ -79,24 +110,6 @@ module LazyCupid
       # end
     end
 
-    def track_msg_dates(msg_page)
-      result = async_response(msg_page)
-      message_list = result[:body].scan(/"message_(\d+)"/)
 
-      message_list.each do |message_id|
-        message_id      = message_id[0]
-        msg_block       = result[:html].parser.xpath("//li[@id='message_#{message_id}']").to_html
-        sender          = /\/([\w\d_-]+)\?cf=messages/.match(msg_block)[1]
-        timestamp_block = result[:html].parser.xpath("//li[@id='message_#{message_id.to_s}']/span/script/text()").to_html
-        timestamp       = timestamp_block.match(/(\d{10}), 'MAI/)[1].to_i
-        sender          = sender.to_s
-        gender          = "Q"
-
-        register_message(sender, timestamp, gender)
-
-      end
-
-
-    end
   end
 end

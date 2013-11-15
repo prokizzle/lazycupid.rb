44ce04ce4638353efb542b693bd7ce2da74bc684
Nick Prokesch <nprokesch@gmail.com>
2013-11-14 20:18:33 -0500

    enhanced ajax match search scraping by randomizing sort order

--- lib/LazyCupid/match_queries.rb
+++ lib/LazyCupid/match_queries.rb
@@ -2,12 +2,20 @@ module LazyCupid
   class MatchQueries
     require 'uri'
 
+    def self.random_sort
+      ['JOIN', 'MATCH', 'SPECIAL_BLEND', 'LOGIN', 'MATCH_AND_NEW', 'MATCH_AND_LOGIN', 'MATCH_AND_DISTANCE'].sample
+    end
+
     def focus_new_users_query
       URI.escape("http://www.okcupid.com/match?filter1=0,34&filter2=2,19,29&filter3=3,25&filter4=5,604800&filter5=1,1&filter6=35,2&filter7=6,604800&filter8=10,0,16510&locid=0&timekey=1&matchOrderBy=JOIN&custom_search=0&fromWhoOnline=0&mygender=m&update_prefs=1&sort_type=0&sa=1&ajax_load=1")
     end
 
+    # def self.default_query
+    #   URI.escape("http://www.okcupid.com/match?timekey=#{Time.now.to_i}&matchOrderBy=MATCH&use_prefs=1&discard_prefs=1&low=11&count=10&&filter7=6,604800&ajax_load=1")
+    # end
+
     def self.default_query
-      URI.escape("http://www.okcupid.com/match?timekey=#{Time.now.to_i}&matchOrderBy=MATCH&use_prefs=1&discard_prefs=1&low=11&count=10&&filter7=6,604800&ajax_load=1")
+      URI.escape("http://www.okcupid.com/match?timekey=#{Time.now.to_i}&matchOrderBy=#{random_sort}&use_prefs=1&discard_prefs=1&low=11&count=10&&filter7=6,604800&ajax_load=1")
     end
 
   end

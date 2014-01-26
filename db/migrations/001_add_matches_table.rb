b724a0d4ff1b035b21f8e98b48daabfaaa24eebd
Nick Prokesch <nprokesch@gmail.com>
2014-01-25 20:50:53 -0500

    added migration for creating matches and stats database tables
    Former-commit-id: 9781613c60035e2709370706f8664703b64de870

---
+++ db/migrations/001_add_matches_table.rb
@@ -0,0 +1,48 @@
+
+Sequel.migration do
+  change do
+    create_table :matches do
+      String :name
+      String :account
+      Integer :counts
+      Boolean :ignored
+      Integer :visitor_timestamp
+      Integer :visit_count
+      Integer :last_visit
+      String :gender
+      String :age
+      String :sexuality
+      String :city
+      String :state
+      Integer :time_added
+      String :added_from
+      Integer :match_percentage
+      Integer :match_percent
+      String :relationship_status
+      Boolean :inactive, :default => false
+      Float :height
+      String :bodytype
+      String :kids
+      Integer :distance
+      Integer :enemy_percent
+      Integer :friend_percent
+      Boolean :drugs
+      Boolean :smokes
+      Boolean :drinks
+      String :ethnicity
+      Integer :last_online
+      Boolean :likes_cats
+      Boolean :likes_dogs
+      Integer :ignore_list
+
+    end
+    create_table :stats do
+    	Integer :total_visits
+    	Integer :total_visitors
+    	Integer :new_users
+    	Integer :total_messages
+    	String :account
+    end
+
+  end
+end

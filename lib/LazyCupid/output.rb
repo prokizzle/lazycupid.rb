cb7b1659d7c4f55492050bc4b44c51959a2430b9
Nick Prokesch <nprokesch@gmail.com>
2014-02-19 20:42:45 -0500

    better readability of console log code; up to date view of user stats post-visit

--- lib/LazyCupid/output.rb
+++ lib/LazyCupid/output.rb
@@ -39,7 +39,21 @@ module LazyCupid
       print @username
       last_online = Time.at(user[:last_online]||0).ago.to_words
       last_online = "Online now" if last_online == "47 years ago"
-      result = {user: user[:handle], gender: user[:gender], age: user[:age], distance: user[:distance], match_percent: user[:match_percentage], enemy: user[:enemy_percentage], friend: user[:friend_percentage], city: user[:city], state: user[:state], sexuality: user[:sexuality], count: @you.visited(user[:handle]), last_online: last_online, fog: user[:fog], kincaid: user[:kincaid]}
+      result = {user: user[:handle], 
+        gender: user[:gender], 
+        age: user[:age], 
+        distance: user[:distance], 
+        match_percent: user[:match_percentage], 
+        enemy: user[:enemy_percentage], 
+        friend: user[:friend_percentage], 
+        city: user[:city], 
+        state: user[:state], 
+        sexuality: user[:sexuality], 
+        count: @you.visited(user[:handle]), 
+        last_online: last_online, 
+        last_visit: @you.last_visited(user[:handle]),
+        fog: user[:fog], 
+        kincaid: user[:kincaid]}
       puts result
     end
 

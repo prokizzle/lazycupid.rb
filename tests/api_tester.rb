8cee4d8a7af86823486b4568ac2c29b4399d53a7
Nick Prokesch <nprokesch@gmail.com>
2014-02-24 19:49:13 -0500

    updated api_tester to work with latest codebase
    Former-commit-id: cd18550a069014e5fe65040d8b4b410555dd19b6

--- tests/api_tester.rb
+++ tests/api_tester.rb
@@ -1,4 +1,4 @@
-require './includes'
+require_relative '../lib/LazyCupid/includes'
 # [todo] - bring up to date with latest changes to event_watcher and browser
 # [fix] - make api_tester work
 # [todo] - do elongated testing against okcupid api
@@ -6,24 +6,21 @@ require './includes'
 # [todo] - rework the already-see-events tracking system (the de-duplicator)
 
 
-@debug = false
+@debug = true
 
 @log        = Logger.new("logs/#{ARGV[0]}_#{Time.now}.log")
 
 @hash       = Hash.new { |hash, key| hash[key] =  0}
 @spotlight  = Hash.new
-@messages   = Hash.new
-@stalks     = Hash.new
 @username   = ARGV[0]
-@people_hash = Hash.new
 
-settings    = Settings.new(:username => @username, :path => File.dirname($0) + '/config/')
+settings    = LazyCupid::Settings.new(:username => @username, :path => File.dirname($0) + '/../config/')
 
-db          = DatabaseMgr.new(:login_name => @username, :settings => settings)
-browser     = Browser.new(:username => @username, :password => ARGV[1], :log => @log)
-tracker     = EventTracker.new(:browser => browser, :database => db, :settings => settings)
-api         = EventWatcher.new(:browser => browser, :tracker => tracker, :logger => Logger.new("logs/#{@username}_#{Time.now}.log"))
-api_events  = APIEvents.new(:tracker => tracker)
+db          = LazyCupid::DatabaseMgr.new(:login_name => @username, :settings => settings)
+browser     = LazyCupid::Browser.new(:username => @username, :password => ARGV[1], :log => @log)
+tracker     = LazyCupid::EventTracker.new(:browser => browser, :database => db, :settings => settings)
+api         = LazyCupid::EventWatcher.new(:browser => browser, :tracker => tracker, :logger => Logger.new("logs/#{@username}_#{Time.now}.log"))
+api_events  = LazyCupid::APIEvents.new(:tracker => tracker)
 print "Logging in... "
 
 if api.login
@@ -32,94 +29,7 @@ else
   puts "Failed."
 end
 
-def print_event_info
-  if @debug
-    puts "--------------------------"
-    print "Event:"
-    p @event
-    print "People:"
-    p @people
-    puts "--------------------------"
-  end
-end
 
-# def msg_notify
-#   puts "New message from #{@people["screenname"]}" unless @messages.has_key?(@event["server_gmt"])
-#   @messages[@event["server_gmt"]] = @people["screenname"]
-#   print_event_info
-# end
-
-# def looks_vote
-
-# end
-
-# def stalk
-#   # unless @event["server_gmt"].to_i == @last_event_time
-#   puts "New visit from #{@people['screenname']}" unless @stalks.has_key?(@event["server_gmt"])
-#   @stalks[@event["server_gmt"]] = @people["screenname"]
-#   # end
-#   # @last_event_time = @event["server_gmt"].to_i
-#   print_event_info
-
-# end
-
-# def new_spotlight_user
-#   unless @spotlight.has_key?(@event["server_gmt"])
-#     puts "New spotlight user: #{@people["screenname"]}"
-#     @spotlight[@event["server_gmt"]] = @people["screenname"]
-#   end
-#   print_event_info
-# end
-
-# def toolbar_trigger
-#   puts "Toolbar trigger"
-#   print_event_info
-# end
-
-# def variable_
-#   events_array.size.to_i + 1
-# end
-
-# def process(event, people)
-#   @event = event
-#   @people = people
-#   type = event["type"]
-#   self.send(type)
-# end
-
-
-# begin
-# @new_events = Array.new
-# @new_people = Array.new
-# hash = Hash.new { |hash, key| hash[key] = 0 }
 loop do
-  # temp = api.poll_response
-
-  # temp["people"].each do |user|
-  #   unless @people_hash.has_key?(user["screenname"])
-  #     @people_hash[user["screenname"]] = user
-  #   end
-  # end
-  # begin
-  #   temp["events"].each do |event|
-  #     key = "#{event['server_gmt']}#{event['type']}"
-  #     unless @hash[key] == event["server_seqid"]
-  #       p "New #{event['type']} From #{event['from']}"
-  #       p "At #{Time.at(event['server_gmt'])}"
-  #       # p event['server_seqid']
-  #       # p @people_hash[event['from']]
-  #       @api.process(event, @people_hash[event['from'])
-  #       # puts "-----------"
-  #       @hash[key] = event["server_seqid"]
-  #     end
-
-  #   end
-  # rescue
-  #   puts "*****"
-  #   p temp["events"]
-  #   puts "*****"
-  # end
-
   api.check_events
-
 end

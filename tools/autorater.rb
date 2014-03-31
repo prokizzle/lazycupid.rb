bacbf2f239e2d190372390ccc511e27298b0eb46
Nick Prokesch <nprokesch@gmail.com>
2014-03-31 11:41:46 -0500

    added standalone autorater tool

---
+++ tools/autorater.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/LazyCupid/auto_rater'
+require 'highline/import'
+
+$verbose = true
+$driver = "chrome"
+if ARGV.size > 0
+  @username = ARGV[0]
+  @password = ARGV[1]
+else
+  @username = ask("username: ")
+  @password = ask("password: "){ |q| q.echo = "*" }
+end
+
+@autorater    = LazyCupid::AutoRater.new(username: @username, password: @password)
+
+@autorater.login
+
+loop do
+  begin
+    @autorater.rate
+    sleep 2
+  rescue SystemExit, Interrupt
+    print "\nGoodbye!"
+    @autorater.logout
+  end
+end
